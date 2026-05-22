35766a84818c6b2489ee5c3a2d1d68ad9f02ea8e
Manish Joshi <mjoshi2233@gmail.com>
2026-05-22 20:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.44%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long bin,i=1;
+        double ans=1.0;
+        bin=n;
+        if (x==1){return 1.0;}
+        if (n==0){return 1.0;}
+        if (x==-1 && n%2==0){return 1.0;}
+         if (x==-1 && n%2==1){return -1.0;}
+        if (n<0){
+            x=1/x;
+            bin=-bin;
+        }
+        while(bin!=0){
+        if (bin>0){
+            if (bin%2==1){
+                ans*=x;
+            }
+        }
+        bin/=2;
+        x*=x;}return ans;
+        
+    }
+};

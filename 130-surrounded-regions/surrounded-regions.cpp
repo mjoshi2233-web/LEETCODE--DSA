6668a34c374a5caa2a88177f364d70fb7a01456d
Manish Joshi <mjoshi2233@gmail.com>
2026-07-27 21:33:52 +0530

    Time: 7 ms (6.31%) | Memory: 21.3 MB (5.10%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
-bool check(int i, int j,
-           vector<vector<char>>& board,
-           vector<vector<bool>>& vis)
-{
-    int m = board.size();
-    int n = board[0].size();
 
-    if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='X')
-        return true;
-
-    if(vis[i][j])
-        return true;
-
-    vis[i][j] = true;
-
-    if(i==0 || j==0 || i==m-1 || j==n-1)
-        return false;
-
-    return check(i+1,j,board,vis) &&
-           check(i-1,j,board,vis) &&
-           check(i,j+1,board,vis) &&
-           check(i,j-1,board,vis);
-}
-void fill(int i, int j, vector<vector<char>>& board)
-{
-    int m = board.size();
-    int n = board[0].size();
-
-    if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='X')
-        return;
-
-    board[i][j] = 'X';
-
-    fill(i+1,j,board);
-    fill(i-1,j,board);
-    fill(i,j+1,board);
-    fill(i,j-1,board);
-}
-    void solve(vector<vector<char>>& board) {
-          int m = board.size();
-    int n = board[0].size();
-
-    for(int i=1;i<m-1;i++)
+    bool check(int i, int j, vector<vector<char>>& temp)
     {
-        for(int j=1;j<n-1;j++)
-        {
-            if(board[i][j]=='O')
-            {
-                vector<vector<bool>> vis(m, vector<bool>(n,false));
+        int m = temp.size();
+        int n = temp[0].size();
 
-                if(check(i,j,board,vis))
+        if(i < 0 || j < 0 || i >= m || j >= n || temp[i][j] == 'X')
+            return true;
+
+        if(i == 0 || j == 0 || i == m-1 || j == n-1)
+            return false;
+
+        temp[i][j] = 'X';
+
+        return check(i+1, j, temp) &&
+               check(i-1, j, temp) &&
+               check(i, j+1, temp) &&
+               check(i, j-1, temp);
+    }
+
+    void fill(int i, int j, vector<vector<char>>& board)
+    {
+        int m = board.size();
+        int n = board[0].size();
+
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == 'X')
+            return;
+
+        board[i][j] = 'X';
+
+        fill(i+1, j, board);
+        fill(i-1, j, board);
+        fill(i, j+1, board);
+        fill(i, j-1, board);
+    }
+
+    void solve(vector<vector<char>>& board) {
+
+        int m = board.size();
+        int n = board[0].size();
+
+        for(int i = 1; i < m-1; i++)
+        {
+            for(int j = 1; j < n-1; j++)
+            {
+                if(board[i][j] == 'O')
                 {
-                    fill(i,j,board);
+                    vector<vector<char>> temp = board;
+
+                    if(check(i, j, temp))
+                    {
+                        fill(i, j, board);
+                    }
                 }
             }
         }
-    }
-        
     }
 };

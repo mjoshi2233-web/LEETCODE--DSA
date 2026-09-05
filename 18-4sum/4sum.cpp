35acaa7d1174f679f527d83ddfde612f22140bdd
Manish Joshi <mjoshi2233@gmail.com>
2026-09-05 12:01:55 +0530

    Time: 339 ms (5.01%) | Memory: 84.1 MB (5.10%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set <vector<int>> s;
-        sort(nums.begin(),nums.end());
-        if(nums.size()<4){return {};}
-        for(int i=0;i<nums.size()-3;i++){
-         for(int j=i+1;j<nums.size()-2;j++){
-            int k=j+1;
-            int l=nums.size()-1;
-            while(k<l){
-                long long summ =nums[i]+nums[j];
-                summ+=nums[k];
-                summ+=nums[l];
+      int n=nums.size();
+      if(n<4){return {};}
+      set <vector<int>> s;
+      sort(nums.begin(),nums.end());
+      for(int i=0;i<n-3;i++){
+        for(int j=i+1;j<n-2;j++){
+            int start=j+1,end=n-1;
+            
+            while(start<end){
+                long long summ=nums[i]+nums[j];
+                summ+=nums[start];
+                summ+=nums[end];
                 if(summ==target){
-                    s.insert({nums[i],nums[j],nums[k],nums[l]});
-                    k++;l--;
+                    s.insert({nums[i],nums[j],nums[start],nums[end]});
                 }
-                else if(summ<target){
-                    k++;
+                if(summ<target){start++;}
+                else{
+                    end--;
                 }
-                else{l--;}
+
             }
-         }
         }
-        return {s.begin(),s.end()};
+      }
+      return {s.begin(),s.end()};
     }
 };

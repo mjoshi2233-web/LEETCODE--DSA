969c25bb7ba6133978891d6dca4db57cc6b70eb0
Manish Joshi <mjoshi2233@gmail.com>
2026-09-13 11:51:00 +0530

    Time: 231 ms (0.00%) | Memory: 15.5 MB (40.00%) - LeetSync

--- 4406-minimum-days-to-score-exactly-n-points/minimum-days-to-score-exactly-n-points.cpp
+++ 4406-minimum-days-to-score-exactly-n-points/minimum-days-to-score-exactly-n-points.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int check(vector<int> &dp,int i,int n,int incre){
-       
-        
-        if(i>n){return 1e9;}
-        if(i==n){return 0;}
-        if(dp[i]!=-1 && incre==1){return dp[i];}
-        int take=1 + check(dp,i+incre,n,incre+1);
-        int skip=1e9;
-        if(incre>1){ skip=1 + check(dp,i,n,1);}
-        if(incre==1){dp[i]=min(take,skip);}
-        return min(take,skip);
-        
-    }
     int minDays(int n) {
-        vector<int> dp(n+2,-1);
-        int ans=check(dp,0,n,1);
-        return ans;
-        
-        
+
+        vector<int> dp(n + 1, 1e9);
+        dp[0] = 0;
+
+        for(int x = 1; x <= n; x++) {
+
+            for(int k = 1; k * (k + 1) / 2 <= x; k++) {
+
+                int triangular = k * (k + 1) / 2;
+
+                if(triangular == x) {
+                    dp[x] = min(dp[x], k);
+                }
+                else {
+                    dp[x] = min(dp[x],
+                                dp[x - triangular] + k + 1);
+                }
+            }
+        }
+
+        return dp[n];
     }
 };
